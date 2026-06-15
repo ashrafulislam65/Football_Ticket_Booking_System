c47a222ca1f5035174005e8fae40d50db4d000e1
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2026-06-15 16:04:07 +0600

    tables creation done

---
+++ tables.sql
@@ -0,0 +1,35 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    role VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20)
+);
+
+CREATE TABLE Matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(100) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price INT NOT NULL,
+    match_status VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT,
+    match_id INT,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(50),
+    total_cost INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (match_id) REFERENCES Matches(match_id)
+);
+
+
+
+
+
+
+
+
+
